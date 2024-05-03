1699b5897684ee3a6208b1e524c32d0cbf7a1526
Boubpha Houngthachack <Boubpha@tplus.la>
2024-05-03 16:52:48 +0700

    add app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// console.log("Hello,Node.js!");
+//  var firstName = 'boubpha';
+//  var lasName = 'houngthachack';
+//  console.log(firstName + "" + lasName);
+
+// let className = "API Development by Node.js";
+// console.log(className);
+
+const emp_id =110;
+console.log("Employee ID:" + emp_id);
